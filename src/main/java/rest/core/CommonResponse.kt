5e15fa1a9cecf334aac1a24e22918e5786096008
sinhro <sinhro-200@mail.ru>
2020-09-10 20:23:03 +0300

    Создал методы для удобного создания ответа и запроса

--- src/main/java/rest/core/CommonResponse.kt
+++ src/main/java/rest/core/CommonResponse.kt
@@ -4,7 +4,24 @@ open class CommonResponse<T>(
         var responseBody: T? = null,
         var error: CommonError? = null
 ) {
+
     companion object
+
+    fun makeError(
+            message: String,
+            description: String? = null,
+            uiMessage: String? = null
+    ): CommonResponse<T> {
+        this.error = CommonError(message, description,uiMessage)
+        return this
+    }
+
+    fun makeSuccess(
+            responseBody: T
+    ): CommonResponse<T> {
+        this.responseBody = responseBody
+        return this
+    }
 
     class CommonError(
             var message: String? = null,
