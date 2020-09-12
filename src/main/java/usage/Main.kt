e11ddec3990846aa90306d59551e213242ea084f
sinhro <sinhro-200@mail.ru>
2020-09-12 16:28:34 +0300

    Изменил название groupid, version, и версию котлина

--- src/main/java/usage/Main.kt
+++ src/main/java/usage/Main.kt
@@ -4,14 +4,18 @@ import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.node.ObjectNode
 import rest.CreateRoomReqData
+import rest.RegisterRespBody
 import rest.core.CommonRequest
+import rest.core.CommonResponse
 
 class Main {
     private fun test() {
         val mapper = ObjectMapper()
         val on: ObjectNode = mapper.createObjectNode()
 
-        val cr = CommonRequest(CreateRoomReqData("testRoom"))
+        var cr:CommonResponse<Any> = CommonResponse.buildError("asd")
+        cr = CommonResponse.buildSuccess(RegisterRespBody("success"))
+
 
         val jsonNode: JsonNode = mapper.valueToTree(cr)
 
