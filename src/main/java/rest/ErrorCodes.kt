a86ebc0e4846f225ff75f817409dc9eca3a45861
sinhro <sinhro-200@mail.ru>
2020-09-28 16:46:48 +0300

    Добавил метод для создания кодов ошибок

--- src/main/java/rest/ErrorCodes.kt
+++ src/main/java/rest/ErrorCodes.kt
@@ -5,3 +5,7 @@ public val ERROR_CODE_REQUEST_DATA_EXC = 1
 public val ERROR_CODE_NIMUSC_EXC = 2
 public val ERROR_CODE_MUSIC_SERVICE_EXC = 3
 public val ERROR_CODE_NETWORK_TIMEOUT = 4
+
+public fun buildErrorCode(MAIN_ERROR_CODE : Int, addErrCode : Int) =
+        (ERROR_CODE_NIMUSC_EXC.toString() + addErrCode).toInt()
+
