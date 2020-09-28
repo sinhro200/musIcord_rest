3dd14324316a251711e5ad69601fbf6883a245da
sinhro <sinhro-200@mail.ru>
2020-09-28 17:07:00 +0300

    Добавил новых кодов, повысил версию

--- src/main/java/rest/ErrorCodes.kt
+++ src/main/java/rest/ErrorCodes.kt
@@ -7,6 +7,12 @@ public val ERROR_CODE_MUSIC_SERVICE_EXC = 3
 public val ERROR_CODE_NETWORK_TIMEOUT = 4
 public val ERROR_CODE_SMTH_WENT_WRONG = 5
 
+public val ERROR_CODE_REGISTER_USER_ALREDY_EXIST = 100
+public val ERROR_CODE_REGISTER_EMAIL_ALREADY_USED = 101
+public val ERROR_CODE_AUTH_USER_NOT_FOUND = 110
+public val ERROR_CODE_AUTH_PASSWORD_INCORRECT = 111
+public val ERROR_CODE_AUTH_USER_NOT_VERIFIED = 112
+
 
 public fun buildErrorCode(MAIN_ERROR_CODE : Int, additionalErrCode : Int? = null) =
         (MAIN_ERROR_CODE.toString() + additionalErrCode?:"00").toInt()
