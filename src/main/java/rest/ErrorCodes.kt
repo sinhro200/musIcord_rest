567f8d5b5c95d08cd076aaa1125161594fb37c48
sinhro <sinhro-200@mail.ru>
2020-09-28 16:54:35 +0300

    исправил метод для создания кодов ошибок 3

--- src/main/java/rest/ErrorCodes.kt
+++ src/main/java/rest/ErrorCodes.kt
@@ -5,7 +5,9 @@ public val ERROR_CODE_REQUEST_DATA_EXC = 1
 public val ERROR_CODE_NIMUSC_EXC = 2
 public val ERROR_CODE_MUSIC_SERVICE_EXC = 3
 public val ERROR_CODE_NETWORK_TIMEOUT = 4
+public val ERROR_CODE_SMTH_WENT_WRONG = 5
 
-public fun buildErrorCode(MAIN_ERROR_CODE : Int, addErrCode : Int? = null) =
-        (MAIN_ERROR_CODE.toString() + "00").toInt()
+
+public fun buildErrorCode(MAIN_ERROR_CODE : Int, additionalErrCode : Int? = null) =
+        (MAIN_ERROR_CODE.toString() + additionalErrCode?:"00").toInt()
 
